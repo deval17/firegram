323349c2f49e2c33481e6d7f2f5001876f51558f
deval17 <35834912+deval17@users.noreply.github.com>
2020-09-23 21:45:21 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,15 @@ import * as firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
 
+//Copy these credentials from firebase
 var firebaseConfig = {
-    apiKey: "AIzaSyBvcGnnxICNrZC77afOtX0xhwlQlyN2Xpk",
-    authDomain: "miu-firegram.firebaseapp.com",
-    databaseURL: "https://miu-firegram.firebaseio.com",
-    projectId: "miu-firegram",
-    storageBucket: "miu-firegram.appspot.com",
-    messagingSenderId: "810144128661",
-    appId: "1:810144128661:web:a44df11978fa4e346a859c"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   // Initialize Firebase
